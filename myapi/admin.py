17b1a42de8f4c849780d43e2160034da893aed9f
William Owalla <william.owalla@student.moringaschool.com>
2022-06-14 14:39:05 +0300

    register models on admin

--- myapi/admin.py
+++ myapi/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Rating, Profile, Post
 
 # Register your models here.
+admin.site.register(Rating)
+admin.site.register(Profile)
+admin.site.register(Post)
