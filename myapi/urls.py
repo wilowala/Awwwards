290559fd7a331d908c3776c64e45bcc73dd15277
William Owalla <william.owalla@student.moringaschool.com>
2022-06-14 22:39:21 +0300

    refactor myapi urls

--- myapi/urls.py
+++ myapi/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.api_overview, name='apiOverview'),
     path('posts/', views.post_list, name='posts'), 
-    path('profiles/', views.profile_list, name='profile'), 
+    path('profiles/', views.profile_list, name='profiles'), 
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
