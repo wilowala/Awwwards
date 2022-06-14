7b892732992ae8e42e796dcdaf632c8147448ec5
William Owalla <william.owalla@student.moringaschool.com>
2022-06-14 22:36:44 +0300

    alter project field

--- myapi/models.py
+++ myapi/models.py
@@ -22,7 +22,7 @@ class Profile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     profile_photo = CloudinaryField("image", blank= True)
     bio = models.TextField(blank=True)
-    projects = models.ForeignKey(Post, on_delete=models.CASCADE, null=True)
+    projects = models.ForeignKey(Post, on_delete=models.CASCADE, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
