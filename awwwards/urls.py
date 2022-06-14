117109b90162403a2f0dab87d872231951d4eb48
William Owalla <william.owalla@student.moringaschool.com>
2022-06-14 15:05:14 +0300

    include myapi urls to project's urls

--- awwwards/urls.py
+++ awwwards/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('awwwards_app.urls')),
+    path('api/', include('myapi.urls')),
 ]
